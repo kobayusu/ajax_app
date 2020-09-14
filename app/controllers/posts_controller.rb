2b2e168cfa5599424870f209fd2bcf05167b3709
kobayusu <cb3ysk@gmail.com>
2020-09-14 14:59:57 +0900

    既読機能サーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,22 @@
 class PostsController < ApplicationController
- def index  # indexアクションを定義した
-  @posts = Post.all.order(id: "DESC") #1番目のレコードを＠postに代入
- end
+  def index  # indexアクションを定義した
+    @posts = Post.all.order(id: "DESC") #1番目のレコードを＠postに代入
+  end
 
- def create
-  Post.create(content: params[:content])
-  redirect_to action: :index
- end
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
